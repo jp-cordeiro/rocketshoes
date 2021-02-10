a11f444af60e5fec4cfbe1922f1fcc4e3a017c6b
JP <jpc.araujo@hotmail.com>
2021-02-09 21:49:10 -0300

    add router, add Cart and Home components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './routes';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes />
+      </div>
+    </BrowserRouter>
   );
 }
 
