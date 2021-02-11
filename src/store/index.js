baa947b73ddd75819182064eb0160fe8b5773eed
JP <jpc.araujo@hotmail.com>
2021-02-10 21:33:17 -0300

    add and config redux-devTools

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,9 @@
-import { createStore } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 import rootReducer from './modules/rootReducer';
 
-const store = createStore(rootReducer);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const enhancer = composeEnhancers(applyMiddleware());
+
+const store = createStore(rootReducer, enhancer);
 
 export default store;
