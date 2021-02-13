74d8bd45157cdbb23d039179a3805db971c58824
JP <jpc.araujo@hotmail.com>
2021-02-13 14:26:15 -0300

    check stock when adding

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -8,8 +8,19 @@ function* addToCart({ id }) {
     const productExists = yield select((state) =>
       state.cartReducer.find((product) => product.id === id)
     );
+
+    const stock = yield call(api.get, `/stock/${id}`);
+    const stockAmount = stock.data.amount;
+    const currentAmount = productExists ? productExists.amount : 0;
+
+    const amount = currentAmount + 1;
+
+    if (amount > stockAmount) {
+      console.log('ERRO');
+      return;
+    }
+
     if (productExists) {
-      const amount = productExists.amount + 1;
       yield put(updateAmount(id, amount));
     } else {
       const response = yield call(api.get, `/products/${id}`);
